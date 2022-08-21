f6c9a2d22cd9b5bb4732bb41b8a7070b872bccef
LeChuong2022 <lenhochuong@gmail.com>
2022-08-22 00:15:38 +0700

    ss15-repair

--- 02.Module 02/src/ss15_xu_ly_ngoai_le_va_debug/exercise/su_dung_lop_illegalTriangle/IllegalTriangleException.java
+++ 02.Module 02/src/ss15_xu_ly_ngoai_le_va_debug/exercise/su_dung_lop_illegalTriangle/IllegalTriangleException.java
@@ -1,4 +1,4 @@
-package ss15_xu_ly_ngoai_le_va_debug.exercise.xu_dung_lop_illegalTriangle;
+package ss15_xu_ly_ngoai_le_va_debug.exercise.su_dung_lop_illegalTriangle;
 
 public class IllegalTriangleException extends Exception {
     public IllegalTriangleException(String message) {
