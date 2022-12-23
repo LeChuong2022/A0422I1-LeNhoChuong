6eda005c2f42ed70e49e33ad388579e7806fcc9d
LeChuong2022 <lenhochuong@gmail.com>
2022-12-23 23:42:00 +0700

    update ss09

--- 08.Module_04/ss09_aop_and_exception_handling/practice/blog-expanse/src/main/java/com/example/blogexpanse/handler/CustomExceptionHandler.java
+++ 08.Module_04/ss09_aop_and_exception_handling/practice/blog-expanse/src/main/java/com/example/blogexpanse/handler/CustomExceptionHandler.java
@@ -5,16 +5,16 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice
 public class CustomExceptionHandler {
-//    @ExceptionHandler(Exception.class)
-//    public String exception(Exception ex){
-//        System.out.println("Lỗi mới nè: " + ex.getMessage());
-//        return "blog/error";
-//    }
-    @ExceptionHandler(IllegalArgumentException.class)
-    public String exception(IllegalArgumentException ex){
+    @ExceptionHandler(Exception.class)
+    public String exception(Exception ex){
         System.out.println("Lỗi mới nè: " + ex.getMessage());
         return "blog/error";
     }
+//    @ExceptionHandler(IllegalArgumentException.class)
+//    public String exception(IllegalArgumentException ex){
+//        System.out.println("Lỗi mới nè: " + ex.getMessage());
+//        return "blog/error";
+//    }
 
     @ExceptionHandler(NullPointerException.class)
     public String exception2(Exception ex){
